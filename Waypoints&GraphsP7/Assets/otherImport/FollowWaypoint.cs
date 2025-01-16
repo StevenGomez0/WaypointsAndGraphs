d3cc8fd9a6d6835a30e9fde76f14c28ebe45aa21
Steven Gomez <719513@student.dusd.net>
2025-01-16 15:21:20 -0800

    navmesh project started
    ok so were just taking the project from graphs and turning it into a navmesh project
    
    should be relatively easy especially since i did some (rudimentary) work with navmeshes for final project

--- Waypoints&GraphsP7/Assets/otherImport/FollowWaypoint.cs
+++ Waypoints&GraphsP7/Assets/otherImport/FollowWaypoint.cs
@@ -3,85 +3,49 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Runtime.InteropServices.WindowsRuntime;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class FollowWaypoint : MonoBehaviour
 {
-    Transform goal;
-    float speed = 5.0f;
-    float accuracy = 2.0f;
-    float rotSpeed = 2.0f;
-
+    NavMeshAgent agent;
     public GameObject wpManager;
     GameObject[] wps;
     GameObject currentNode;
-    [SerializeField] int currentWP = 0;
-    Graph g;
 
     // Start is called before the first frame update
     void Start()
     {
+        agent = GetComponent<NavMeshAgent>();
         Time.timeScale = 5;
         Debug.Log("started");
         wps = wpManager.GetComponent<WaypointManager>().waypoints;
-        g = wpManager.GetComponent<WaypointManager>().graph;
-        currentNode = wps[0];
 
         //Invoke("GoToRuin", 2);
     }
 
     public void GoToHeli()
     {
-        g.AStar(currentNode, wps[0]);
-        currentWP = 0;
+        agent.SetDestination(wps[0].transform.position);
     }
 
     public void GoToRuin()
     {
-        Debug.Log("gotoruin invoked");
-        g.AStar(currentNode, wps[16]);
-        currentWP = 0;
+        agent.SetDestination(wps[16].transform.position);
     }
 
     public void GoToFactory()
     {
-        g.AStar(currentNode, wps[12]);
-        currentWP = 0;
+        agent.SetDestination(wps[12].transform.position);
     }
 
     public void GoToSomewhere()
     {
-        g.AStar(currentNode, wps[5]);
-        currentWP = 0;
+        agent.SetDestination(wps[5].transform.position);
     }
 
     // Update is called once per frame
     void LateUpdate()
     {
-        Debug.Log("lateupdate called");
-        if (g.pathList.Count == 0 || currentWP == g.pathList.Count)
-        {
-            Debug.Log("return called");
-            return;
-        }
-
-        if (Vector3.Distance(g.pathList[currentWP].getId().transform.position, transform.position) < accuracy)
-        {
-            Debug.Log("currentNode updated, current wp updated");
-            currentNode = g.pathList[currentWP].getId();
-            currentWP++;
-        }
-
-        if (currentWP < g.pathList.Count)
-        {
-            Debug.Log("lookat and transform called");
-            goal = g.pathList[currentWP].getId().transform;
-            Vector3 lookAtGoal = new Vector3(goal.position.x, transform.position.y, goal.position.z);
-
-            Vector3 direction = lookAtGoal - transform.position;
-            transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(direction), Time.deltaTime * rotSpeed);
-
-            transform.Translate(0, 0, speed * Time.deltaTime);
-            Debug.Log("translating");
-        }
+        
     }
 }
